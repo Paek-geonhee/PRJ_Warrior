ac06d2eb6f454aaab8c9c3a193777472913aa051
Paek-geonhee <74587450+Paek-geonhee@users.noreply.github.com>
2025-04-10 15:08:57 +0900

    0410-1
    적 공격 기능 추가
    
    - 공격 시도 확률 계산 기능 도입 -> Strafing 도중 공격 시도 / 확률에 의해 공격을 시도하거나 공격하지 않음
    - 적 공격 능력 추가 (1형태, 2형태)
    - 각 능력별 몽타주 도입 -> strafing 도중 공격 시도에 성공하면 플레이어 가까이 다가와 공격 모션을 재생.
    
    -> 공격 모션 중 무기와 플레이어 간 충돌을 감지하고 데미지 로직을 적용해야 함.

--- Warrior/Intermediate/Build/BuildRulesProjects/WarriorModuleRules/obj/Development/WarriorModuleRules.AssemblyInfo.cs
+++ Warrior/Intermediate/Build/BuildRulesProjects/WarriorModuleRules/obj/Development/WarriorModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WarriorModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+591ccf4aacd270953bf0fce254cd3cb7184733d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c6aa75141b6dc18e9d669c267452748ab262720")]
 [assembly: System.Reflection.AssemblyProductAttribute("WarriorModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WarriorModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Warrior/Source/Warrior/Private/AbilitySystem/WarriorAbilitySystemComponent.cpp
+++ Warrior/Source/Warrior/Private/AbilitySystem/WarriorAbilitySystemComponent.cpp
@@ -2,7 +2,9 @@
 
 
 #include "AbilitySystem/WarriorAbilitySystemComponent.h"
-#include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
+#include "AbilitySystem/Abilities/WarriorHeroGameplayAbility.h"
+
+#include "WarriorDebugHelper.h"
 
 void UWarriorAbilitySystemComponent::OnAbilityInputPressed(FGameplayTag InputTag)
 {
@@ -46,4 +48,28 @@ void UWarriorAbilitySystemComponent::RemoveGrantedHeroWeaponAbilities(UPARAM(ref
 
 	InAbilitySpecHandles.Empty();
 
+}
+
+bool UWarriorAbilitySystemComponent::TryActivateAbilityByTag(FGameplayTag AbilityTagToActivate)
+{
+	check(AbilityTagToActivate.IsValid());
+
+
+	TArray<FGameplayAbilitySpec*> FoundAbilitySpecs;
+	GetActivatableGameplayAbilitySpecsByAllMatchingTags(AbilityTagToActivate.GetSingleTagContainer(), FoundAbilitySpecs);
+
+	if (!FoundAbilitySpecs.IsEmpty()) {
+		const int32 RandomAbilityIndex = FMath::RandRange(0, FoundAbilitySpecs.Num() - 1);
+		FGameplayAbilitySpec* SpecToActivate = FoundAbilitySpecs[RandomAbilityIndex];
+
+		check(SpecToActivate);
+
+		Debug::Print(TEXT("Found Ability"), FColor::Red);
+
+		if (!SpecToActivate->IsActive()) {
+			return TryActivateAbility(SpecToActivate->Handle);
+		}
+	}
+
+	return false;
 }
