491c919c1068eb210693db2e09c13126b76fdad6
Paek-geonhee <74587450+Paek-geonhee@users.noreply.github.com>
2025-05-09 16:32:24 +0900

    0509
    원거리 적 추가 기능 구현
    
    - 근접 공격 ( 방어 불가 )
    - 근접 공격 이펙트, 사운드
    
    + 보스 구현을 위한 기초 세팅 (BP, Anim, Stat 처리)

--- Warrior/Source/Warrior/Private/WarriorGameplayTags.cpp
+++ Warrior/Source/Warrior/Private/WarriorGameplayTags.cpp
@@ -57,8 +57,8 @@ namespace WarriorGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(Enemy_Ability_Ranged, "Enemy.Ability.Ranged");
 
 	UE_DEFINE_GAMEPLAY_TAG(Enemy_Status_Strafing, "Enemy.Status.Strafing");
-	UE_DEFINE_GAMEPLAY_TAG(Enemy_Status_UnderAttack, "Enemy.Status.UnderAttack");
-
+	UE_DEFINE_GAMEPLAY_TAG(Enemy_Status_UnderAttack, "Enemy.Status.UnderAttack"); 
+	UE_DEFINE_GAMEPLAY_TAG(Enemy_Status_Unblockable, "Enemy.Status.Unblockable");
 	
 	UE_DEFINE_GAMEPLAY_TAG(Shared_Event_MeleeHit, "Shared.Event.MeleeHit");
 	UE_DEFINE_GAMEPLAY_TAG(Shared_Event_HitReact, "Shared.Event.HitReact"); 
